a1d074141f08a18ef7071e1e51852b0235ad8bd5
Felipe Borges <69490840+FelipeB0rges@users.noreply.github.com>
2021-02-02 21:02:56 -0300

    bb

--- header.php
+++ header.php
@@ -12,15 +12,26 @@
 
 <body>
     <header>
-   <div class="container">
-   <a href="?pagina=home"><img src="inter.png" title="logo" alt="logo" style="width: 100px; height:100px"></a>
-    <div class="menu" style="  display: flex;
+        <div class="container">
+            <a href="?pagina=home"><img src="inter.png" title="logo" alt="logo" style="width: 100px; height:100px"></a>
+            <div class="menu" style="  display: flex;
     justify-content: space-between;
     margin-left: 100px;">
-    <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;"><a href="?pagina=cursos" style="color:white; text-decoration:none">CURSOS</a></div>
-    <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;"><a href="?pagina=alunos" style="color:white; text-decoration:none">ALUNOS</a></div>
-    <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;"><a href="?pagina=matriculas" style="color:white; text-decoration:none">MATRICULAS</a></div>
-    
+                <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;">
+                    <a href="?pagina=cursos" style="color:white; text-decoration:none">CURSOS</a>
+                </div>
+                <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;">
+                    <a href="?pagina=alunos" style="color:white; text-decoration:none">ALUNOS</a>
+                </div>
+                <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;">
+                    <a href="?pagina=matriculas" style="color:white; text-decoration:none">MATRICULAS</a>
+                </div>
+                <?php if (isset($_SESSION['login'])) { ?>
+                    <div class="item" style="margin: 0 50px; color:white; margin-top:40px; cursor:pointer;">
+                        <a href="logout.php" style="color:white; text-decoration:none"><?php echo $_SESSION['usuario'] ; ?>(sair)</a>
+                    </div>
+                <?php } ?>
+
     </header>
     <div class="conteudo">
 </body>
--- index.php
+++ index.php
@@ -2,25 +2,56 @@
 #BASE DE DADOS
 include 'db.php';
 
+#iniciar sessão
+
+session_start();
+
 #CABEÇALHO
 include 'header.php';
 
 
-$pagina = $_GET['pagina'];
+if (isset($_SESSION['login'])) {
+    if (isset($_GET['pagina'])) {
+        $pagina = $_GET['pagina'];
+    } else {
+        $pagina = 'cursos';
+    }
+}else{
+    $pagina='home';
+}
 
-switch($pagina){
- case 'cursos': include 'views/cursos.php'; break;
- case 'inserir_cursos'; include 'views/inserir_curso.php'; break;
- case 'matriculas'; include 'views/matriculas.php'; break;
- case 'inserir_matricula' : include 'views/inserir_matricula.php'; break;
- case 'alunos' : include 'views/alunos.php'; break;
- case 'inserir_alunos' :  include 'views/inserir_alunos.php'; break;
- case 'inserir_matricula': include 'views/inserir_matricula.php'; break;
- case 'inserir_curso':  include 'views/inserir_curso.php'; break;
- default:   include 'views/home.php';
+
+
+
+switch ($pagina) {
+    case 'cursos':
+        include 'views/cursos.php';
+        break;
+    case 'inserir_cursos';
+        include 'views/inserir_curso.php';
+        break;
+    case 'matriculas';
+        include 'views/matriculas.php';
+        break;
+    case 'inserir_matricula':
+        include 'views/inserir_matricula.php';
+        break;
+    case 'alunos':
+        include 'views/alunos.php';
+        break;
+    case 'inserir_alunos':
+        include 'views/inserir_alunos.php';
+        break;
+    case 'inserir_matricula':
+        include 'views/inserir_matricula.php';
+        break;
+    case 'inserir_curso':
+        include 'views/inserir_curso.php';
+        break;
+    default:
+        include 'views/home.php';
 }
 
 
 # Rodapé
 include 'footer.php';
-

9f632d90a562e15c54678b71c28baf06ac102508
DavidChagas <36827231+DavidChagas@users.noreply.github.com>
2021-10-29 13:40:05 -0300

    correção

--- views/inserir_alunos.php
+++ views/inserir_alunos.php
@@ -13,4 +13,17 @@
         <input type="text" name="cpf" maxlength="11">
         <button type="submit">Cadastrar</button>
     </form>
+     <form method="post" action="processa_aluno.php">
+        <h4>Nome do Aluno</h4>
+        <input type="text" name="nome">
+        <h4>Endereço</h4>
+        <input type="text" name="endereco">
+        <h4>Estado</h4>
+        <input type="text" name="estado" maxlength="2">
+        <h4>Cidade</h4>
+        <input type="text" name="cidade">
+        <h4>CPF</h4>
+        <input type="text" name="cpf" maxlength="11">
+        <button type="submit">Cadastrar</button>
+    </form>
 </div>
